c00afcc90a66cd456bebdf98a515a85fb9256aa8
hi-dhl <hihuilong@gmail.com>
2021-06-20 17:40:39 +0800

    feat: add Snackbar extension

--- ktkit/src/main/java/com/hi/dhl/ktkit/ui/ViewExt.kt
+++ ktkit/src/main/java/com/hi/dhl/ktkit/ui/ViewExt.kt
@@ -1,6 +1,7 @@
 package com.hi.dhl.ktkit.ui
 
 import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * <pre>
@@ -15,3 +16,21 @@ fun View.visible() = View.VISIBLE
 fun View.gone() = View.GONE
 
 fun View.invisible() = View.INVISIBLE
+
+fun View.showShotSnackbar(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+}
+
+fun View.showLongSnackbar(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
+}
+
+fun View.snackBarWithAction(
+    message: String, actionlable: String,
+    block: () -> Unit
+) {
+    Snackbar.make(this, message, Snackbar.LENGTH_LONG)
+        .setAction(actionlable) {
+            block()
+        }
+}
